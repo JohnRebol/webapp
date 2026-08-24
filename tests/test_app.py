eb6a1c71815c92a25a919e9e4f9e1307fb474eec
jreboll <jonatanrebollar@gmail.com>
2026-08-24 18:58:30 +0000

    Add pytest-cov dependency and create initial test for home page

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from app import app
+
+
+def test_home_page_loads():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert b"Bedrock &amp; Beam" in response.data
