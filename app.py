934a421567f0aaebac2c07eb46092ef6e8e17878
jreboll <jonatanrebollar@gmail.com>
2026-09-25 15:03:26 +0000

    Set Flask app to run in production mode by disabling debug

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def index():
 
 if __name__ == "__main__":
     # host=0.0.0.0 so it's reachable from outside the container
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
